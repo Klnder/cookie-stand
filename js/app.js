777576e7a6dd2d937874ad0fdf275000e2f577c4
Jeremy Merlin <jeremymerlin@hotmail.fr>
2023-04-04 21:20:58 +0100

    make js MORE SEXY

--- js/app.js
+++ js/app.js
@@ -1,21 +1,15 @@
 "use strict";
 
-const hours = [
-  "6am",
-  "7am",
-  "8am",
-  "9am",
-  "10am",
-  "11am",
-  "12pm",
-  "1pm",
-  "2pm",
-  "3pm",
-  "4pm",
-  "5pm",
-  "6pm",
-  "7pm",
+const hours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"];
+const shopInformation = [
+  { location: "Seattle", minCustomer: 23, maxCustomer: 65, avgCookie: 6.3 },
+  { location: "Tokyo", minCustomer: 3, maxCustomer: 24, avgCookie: 1.2 },
+  { location: "Dubai", minCustomer: 11, maxCustomer: 38, avgCookie: 3.7 },
+  { location: "Paris", minCustomer: 20, maxCustomer: 38, avgCookie: 2.3 },
+  { location: "Lima", minCustomer: 2, maxCustomer: 16, avgCookie: 4.6 },
 ];
+
+const shopList = [];
 const parentElement = document.getElementById("list-shop");
 
 function shopCreator(location, minCustomer, maxCustomer, avgCookie) {
@@ -28,10 +22,7 @@ function shopCreator(location, minCustomer, maxCustomer, avgCookie) {
     totalSales: 0,
     calculateSales() {
       for (let i = 0; i < hours.length; i++) {
-        let cookieSale = Math.round(
-          randomCustomerNumber(this.minCustomer, this.maxCustomer) *
-            this.avgCookie
-        );
+        let cookieSale = Math.round(randomCustomerNumber(this.minCustomer, this.maxCustomer) * this.avgCookie);
         this.totalSales += cookieSale;
         this.saleByHour.push(cookieSale);
       }
@@ -58,17 +49,16 @@ function shopCreator(location, minCustomer, maxCustomer, avgCookie) {
   return shop;
 }
 
-const seattle = shopCreator("Seattle", 23, 65, 6.3);
-const tokyo = shopCreator("Tokyo", 3, 24, 1.2);
-const dubai = shopCreator("Dubai", 11, 38, 3.7);
-const paris = shopCreator("Paris", 20, 38, 2.3);
-const lima = shopCreator("Lima", 2, 16, 4.6);
-
-seattle.render();
-tokyo.render();
-dubai.render();
-paris.render();
-lima.render();
+for (let i = 0; i < shopInformation.length; i++) {
+  const tempShop = shopCreator(
+    shopInformation[i].location,
+    shopInformation[i].minCustomer,
+    shopInformation[i].maxCustomer,
+    shopInformation[i].avgCookie
+  );
+  shopList[i] = tempShop;
+  tempShop.render();
+}
 
 function randomCustomerNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
